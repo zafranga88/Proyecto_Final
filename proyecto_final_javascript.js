53e75ec6e2a611d09d5d000aeaa3b300644e9660
zafranga88 <122698284+zafranga88@users.noreply.github.com>
2023-03-01 11:07:23 -0300

    Add files via upload

---
+++ proyecto_final_javascript.js
@@ -0,0 +1,13 @@
+
+document.getElementById('demoButton').addEventListener('click', function () {
+    
+        document.getElementById("demo").innerHTML = "@Giacomo_Piedilunghi63";
+      
+});
+
+document.getElementById('demoButton2').addEventListener('click', function () {
+    
+    document.getElementById("demo2").innerHTML = "PiedilunghiGiacomo@hotmail.com.it";
+  
+});
+
